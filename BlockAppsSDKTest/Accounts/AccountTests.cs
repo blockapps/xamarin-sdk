9f3f101c7b1890d1ca5fd89f433c95299e8bae1b
charlescrain <craincharles@gmail.com>
2016-06-03 15:59:34 -0400

    Unit tests for Accounts and Blocks

---
+++ BlockAppsSDKTest/Accounts/AccountTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using BlockAppsSDK;
+using BlockAppsSDK.Users;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BlockAppsSDKTest.Accounts
+{
+    [TestClass]
+    public class AccountTests
+    {
+        [TestMethod]
+        public async Task CreateAndGetAccount()
+        {
+            //ConnectionString.StratoUrl = "http://xamarintest.centralus.cloudapp.azure.com/strato-single/eth/v1.1/";
+            //ConnectionString.BlocUrl = "http://xamarintest.centralus.cloudapp.azure.com:8000";
+            ConnectionString.BlocUrl = "http://13.93.154.77:8000";
+            //Create an Account to query
+            var newAccount = await Account.CreateAccount("accountTest", "test", true);
+            var account = await Account.GetAccount(newAccount.Address);
+            Assert.AreEqual(newAccount, account);
+        }
+    }
+}
---
+++ BlockAppsSDKTest/Blocks/BlockTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using BlockAppsSDK;
+using BlockAppsSDK.Blocks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BlockAppsSDKTest.Blocks
+{
+    [TestClass]
+    public class BlockTests
+    {
+        [TestMethod]
+        public async Task GetBlock()
+        {
+            ConnectionString.StratoUrl = "http://xamarintest.centralus.cloudapp.azure.com/strato-single/eth/v1.1/";
+            ConnectionString.BlocUrl = "http://xamarintest.centralus.cloudapp.azure.com:8000";
+            var block0 = await Block.GetBlock(0);
+            Console.WriteLine("Block 0 has Parent Hash: " + block0.BlockData.ParentHash);
+        }
+    }
+}
