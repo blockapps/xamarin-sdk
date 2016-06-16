54ffae70dd49bc4c3a95abe6620ab05b9bb04f11
charlescrain <craincharles@gmail.com>
2016-06-16 11:33:01 -0400

    Added better Unit tests for Account and Block

--- BlockAppsSDKTest/Accounts/AccountTests.cs
+++ BlockAppsSDKTest/Accounts/AccountTests.cs
@@ -11,30 +11,32 @@ namespace BlockAppsSDKTest.Accounts
     [TestClass]
     public class AccountTests
     {
+        private AccountManager AccountManager { get; set; }
         [TestMethod]
         public async Task CreateAndGetAccount()
         {
-            //ConnectionString.StratoUrl = "http://xamarintest.centralus.cloudapp.azure.com/strato-single/eth/v1.1/";
-            //ConnectionString.BlocUrl = "http://xamarintest.centralus.cloudapp.azure.com:8000";
-            ConnectionString.BlocUrl = "http://13.93.154.77:8000";
             //Create an Account to query
-            var newAccount = await Account.CreateAccount("accountTest", "test", true);
-            var account = await Account.GetAccount(newAccount.Address);
+            var newAccount = await AccountManager.CreateAccount("accountTest", "test", true);
+            var account = await AccountManager.GetAccount(newAccount.Address);
             Assert.AreEqual(newAccount, account);
         }
 
         [TestMethod]
         public async Task GetAllUserAccounts()
         {
-            //ConnectionString.StratoUrl = "http://xamarintest.centralus.cloudapp.azure.com/strato-single/eth/v1.1/";
-            //ConnectionString.BlocUrl = "http://xamarintest.centralus.cloudapp.azure.com:8000";
-            ConnectionString.BlocUrl = "http://13.93.154.77:8000";
             //Create an Account to query
-            var allAccountAddresses = await Account.GetAccountAddresses();
-            List<Task<Account>> allAccountsTask = allAccountAddresses.Select(async addr => await Account.GetAccount(addr)).ToList();
+            var allAccountAddresses = await AccountManager.GetAccountAddresses();
+            List<Task<Account>> allAccountsTask = allAccountAddresses.Select(async addr => await AccountManager.GetAccount(addr)).ToList();
             
             var allAccounts = (await Task.WhenAll(allAccountsTask)).ToList();
             Assert.IsNotNull(allAccounts);
+        }
+
+        [TestInitialize]
+        public void SetupManagers()
+        {
+           AccountManager = new AccountManager(new Connection("http://40.118.255.235:8000",
+                "http://40.118.255.235/eth/v1.2")); 
         }
     }
 }
--- BlockAppsSDKTest/Blocks/BlockTests.cs
+++ BlockAppsSDKTest/Blocks/BlockTests.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using BlockAppsSDK;
 using BlockAppsSDK.Blocks;
+using BlockAppsSDK.Users;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace BlockAppsSDKTest.Blocks
@@ -9,13 +10,20 @@ namespace BlockAppsSDKTest.Blocks
     [TestClass]
     public class BlockTests
     {
+        private BlockManager BlockManager { get; set; }
+
         [TestMethod]
         public async Task GetBlock()
         {
-            ConnectionString.StratoUrl = "http://xamarintest.centralus.cloudapp.azure.com/strato-single/eth/v1.1/";
-            ConnectionString.BlocUrl = "http://xamarintest.centralus.cloudapp.azure.com:8000";
-            var block0 = await Block.GetBlock(0);
+            var block0 = await BlockManager.GetBlock(0);
             Console.WriteLine("Block 0 has Parent Hash: " + block0.BlockData.ParentHash);
+        }
+
+        [TestInitialize]
+        public void SetupManagers()
+        {
+            BlockManager = new BlockManager(new Connection("http://40.118.255.235:8000",
+                 "http://40.118.255.235/eth/v1.2"));
         }
     }
 }
